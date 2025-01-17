453b4d1cedc9bc65a6daa5b7ca968c384898e67d
shaarmann <stephan.haarmann@camunda.com>
2025-01-17 13:33:46 +0100

    Improved error handling (#49)
    * Improved error handling
    
    - include rudimentary error handling
      - use `set -e` and `set -eo pipefail` if appropriate
      - use && in other cases
    - fix permission issue with zeebe backup jobs
    
    * Further improvements to error handling
    
    * Ran ShellCheck
    
    - executed ShellCheck and corrected all findings:

--- 03 - Lab 3 - Backup and Restore/backup/create-backupId-as-secret.sh
+++ 03 - Lab 3 - Backup and Restore/backup/create-backupId-as-secret.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Generate the backupTimeId based on the current date and time
@@ -7,12 +6,12 @@ backupTimeId=$(date +"%Y%m%d%H%M%S")
 # Check if the secret exists
 kubectl get secret backup-timeid >/dev/null 2>&1
 
-if [ $? -eq 0 ]; then
+if kubectl get secret backup-timeid >/dev/null 2>&1; then
     # Secret exists, update it
-    kubectl create secret generic backup-timeid --from-literal=backupTimeId=$backupTimeId --dry-run=client -o yaml | kubectl apply -f -
+    kubectl create secret generic backup-timeid --from-literal=backupTimeId="$backupTimeId" --dry-run=client -o yaml | kubectl apply -f -
     echo "Updated secret with backupTimeId: $backupTimeId"
 else
     # Secret does not exist, create it
-    kubectl create secret generic backup-timeid --from-literal=backupTimeId=$backupTimeId
+    kubectl create secret generic backup-timeid --from-literal=backupTimeId="$backupTimeId"
     echo "Created secret with backupTimeId: $backupTimeId"
 fi
